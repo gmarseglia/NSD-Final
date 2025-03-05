3513bbab8ba790bdbfe3b9175b326d60ec3759af
gmarseglia <g.marseglia.it@gmail.com>
2025-03-05 17:55:40 +0100

    CE networking

--- scripts/VPN Site 1/CE1/init.sh
+++ scripts/VPN Site 1/CE1/init.sh
@@ -3,7 +3,7 @@
 ## Initialize networking
 ip addr add 192.168.1.1/24 dev eth1
 ip addr add 10.1.1.2/30 dev eth0
-ip route add default via 10.1.1.1 dev eth0
+ip route add default via 10.1.1.1
 
 ## Conclude
 cd
---
+++ scripts/VPN Site 3/CE3/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Initialize networking
+ip addr add 192.168.3.1/24 dev eth1
+ip addr add 10.1.3.2/30 dev eth0
+ip route add default via 10.1.3.1
+
+## Conclude
+cd
+bash
