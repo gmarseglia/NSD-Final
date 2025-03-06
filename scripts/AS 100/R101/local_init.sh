982bc5eec8d927128bf7521f3b76dd09e0112016
gmarseglia <g.marseglia.it@gmail.com>
2025-03-06 15:41:55 +0100

    AS100 initial pre-MPLS setup

---
+++ scripts/AS 100/R101/local_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Create the VRF
+ip link add vpn10 type vrf table 10
+ip link set vpn10 up
+ip link set eth0 master vpn10
+
+## Pre MPLS setup
+echo 'net.mpls.conf.lo.input = 1' >> /etc/sysctl.conf
+echo 'net.mpls.conf.eth1.input = 1' >> /etc/sysctl.conf
+echo 'net.mpls.conf.vpn10.input = 1' >> /etc/sysctl.conf
+echo 'net.mpls.platform_labels = 100000' >> /etc/sysctl.conf
+
+sysctl -p
--- scripts/AS 100/general/init.sh
+++ scripts/AS 100/general/init.sh
@@ -3,6 +3,9 @@
 ulimit -Sn 100000
 /usr/lib/frr/watchfrr zebra bgpd ldpd &
 sleep 1
+if [ -e /root/local_init.sh ]; then
+    source /root/local_init.sh
+fi
 vtysh < /root/commands.txt
 cd
 bash
