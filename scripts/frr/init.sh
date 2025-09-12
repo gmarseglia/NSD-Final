37d7bc81241125dfb2629e9b8ef2cc0913cfcd8e
gmarseglia <g.marseglia.it@gmail.com>
2025-09-12 12:30:44 +0200

    checkpoint before frr.conf refactor

--- scripts/frr/init.sh
+++ scripts/frr/init.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Turn on frr
 ulimit -Sn 100000
 /usr/lib/frr/watchfrr zebra bgpd ldpd &
 sleep 1
+
+# Execute local_init.sh if present
 if [ -e /root/local_init.sh ]; then
     source /root/local_init.sh
 fi
-vtysh < /root/commands.txt
+
+# Load frr.conf
+vtysh -f /root/frr.conf
+
+# Oper terminal
 cd
 bash
