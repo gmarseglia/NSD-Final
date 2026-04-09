547d89b52472449f26e06277f84f3aa956256d56
gmarseglia <g.marseglia.it@gmail.com>
2026-04-09 17:19:01 +0200

    New: removed R104 scripts

--- scripts/AS 100/R104/local_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Pre MPLS setup
-sysctl -w net.mpls.conf.lo.input=1
-sysctl -w net.mpls.conf.eth1.input=1
-sysctl -w net.mpls.conf.eth2.input=1
-sysctl -w net.mpls.conf.eth3.input=1
-sysctl -w net.mpls.platform_labels=100000
