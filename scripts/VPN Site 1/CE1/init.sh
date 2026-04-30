f7497bf07ddc36675a2deafeba35df84b96e9186
gmarseglia <g.marseglia.it@gmail.com>
2026-04-30 13:01:22 +0200

    New: OSPF and BGP in AS100

---
+++ scripts/VPN Site 1/CE1/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Initialize networking
+ip addr add 10.1.1.2/30 dev eth1
+ip addr add 192.168.1.1/24 dev eth0
+ip route add default via 10.1.1.1
+
+sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
+
+## Conclude
+cd
+exec bash
