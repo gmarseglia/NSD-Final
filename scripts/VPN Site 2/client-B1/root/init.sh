c8088f2cb780bb24d4c7931d2870564a43df1f94
gmarseglia <g.marseglia.it@gmail.com>
2026-05-06 18:00:04 +0200

    New: better wpa_cli for Client-B1

--- scripts/VPN Site 2/client-B1/root/init.sh
+++ scripts/VPN Site 2/client-B1/root/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ip addr add  192.168.32.2/24 dev eth0
-ip route add default via 192.168.32.1
+ip route add default via 192.168.32.1 dev eth0
 
 cd
 exec bash
 
-# wpa_supplicant -c /etc/wpa_supplicant.conf -D wired -i eth0
+# wpa_supplicant -c /etc/wpa_supplicant.conf -D wired -i eth0 -B
+# wpa_cli -i eth0 logoff
+# wpa_cli -i eth0 logon
