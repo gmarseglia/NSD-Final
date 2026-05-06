977dbfe4e967c1fc0413da12d4fa58fe8c5da413
gmarseglia <g.marseglia.it@gmail.com>
2026-05-06 10:45:04 +0200

    New: CE2 has interfaces for VLANs

--- scripts/VPN Site 2/CE2/root/init.sh
+++ scripts/VPN Site 2/CE2/root/init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 ## Initialize networking
+ip link add link eth1 name eth0.32 type vlan id 32
+ip link add link eth1 name eth0.95 type vlan id 95
+ip link set eth0.32 up
+ip link set eth0.95 up
+
 ip addr add 10.1.2.2/30 dev eth1
 ip addr add 192.168.2.1/24 dev eth0
+ip addr add 192.168.32.1/24 dev eth0.32
+ip addr add 192.168.95.1/24 dev eth0.95
+
 ip route add default via 10.1.2.1
 
 sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
