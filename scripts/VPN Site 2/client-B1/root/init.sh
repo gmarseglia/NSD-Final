e1dc98e21dd90d05158f10204e5f127fc4e91682
gmarseglia <g.marseglia.it@gmail.com>
2026-05-06 11:03:05 +0200

    Fix: changed Client-Bx ip addr

--- scripts/VPN Site 2/client-B1/root/init.sh
+++ scripts/VPN Site 2/client-B1/root/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip addr add  192.168.32.1/24 dev eth0
+ip addr add  192.168.32.2/24 dev eth0
 ip route add default via 192.168.32.1
 
 cd
--- scripts/VPN Site 2/client-B2/root/init.sh
+++ scripts/VPN Site 2/client-B2/root/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip addr add  192.168.95.1/24 dev eth0
+ip addr add  192.168.95.2/24 dev eth0
 ip route add default via 192.168.95.1
 
 cd
