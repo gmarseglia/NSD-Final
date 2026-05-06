efaf152a4f9c5898423bd19fe2658f31b70e81b1
gmarseglia <g.marseglia.it@gmail.com>
2026-05-06 11:11:01 +0200

    New: static VLAN configuration in eBPF-switch

--- scripts/VPN Site 2/eBPF-Switch/root/init.sh
+++ scripts/VPN Site 2/eBPF-Switch/root/init.sh
@@ -2,11 +2,18 @@
 
 ## Initialize networking
 ip link add name br-auth type bridge
+ip link set dev br-auth type bridge vlan_filtering 1
 
 # Add all relevant interfaces to the bridge, including the uplink (eth0)
 ip link set dev eth0 master br-auth
 ip link set dev eth1 master br-auth
 ip link set dev eth2 master br-auth
+
+# TEMP: set statically VLAN-port association (#TODO: remove)
+bridge vlan add dev eth1 vid 32 pvid untagged
+bridge vlan add dev eth2 vid 95 pvid untagged
+bridge vlan add dev eth2 vid 32
+bridge vlan add dev eth2 vid 95
 
 # Bring up the physical interfaces and the bridge
 ip link set dev eth0 up
@@ -14,16 +21,15 @@ ip link set dev eth1 up
 ip link set dev eth2 up
 ip link set br-auth up
 
-# Forward EAPOL frames (for 802.1X if you are using hostapd)
-echo 8 > /sys/class/net/br-auth/bridge/group_fwd_mask
-
-ebtables -P FORWARD DROP
-ebtables -P INPUT ACCEPT
-ebtables -P OUTPUT ACCEPT
-
 # Assign the IP address and default route to the BRIDGE interface, not eth0
 ip addr add 192.168.2.2/24 dev br-auth
 ip route add default via 192.168.2.1 dev br-auth
+
+# Forward EAPOL frames (for 802.1X if you are using hostapd)
+# echo 8 > /sys/class/net/br-auth/bridge/group_fwd_mask
+# ebtables -P FORWARD DROP
+# ebtables -P INPUT ACCEPT
+# ebtables -P OUTPUT ACCEPT
 
 ## Conclude
 cd
