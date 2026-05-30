412476c56f03262adaa820d44a828da9d68e11cb
gmarseglia <g.marseglia.it@gmail.com>
2026-05-30 13:19:37 +0200

    Refactor: Client A1 files

--- scripts/VPN Site 1/client-A1/root/init.sh
+++ scripts/VPN Site 1/client-A1/root/init.sh
@@ -4,6 +4,7 @@
 ip addr add 192.168.1.2/24 dev ens33
 ip route add default via 192.168.1.1
 
-# apparmor_parser -r /etc/apparmor.d/usr.sbin.proftpd
+apparmor_parser -r /etc/apparmor.d/usr.sbin.proftpd
 # aa-enforce /usr/sbin/proftpd
-# systemctl restart proftpd.service
+systemctl restart proftpd.service
+aa-status --filter.exe=/usr/sbin/proftpd
