d631e29adef703f8ac17a7616d4edff57f5e3b72
gmarseglia <g.marseglia.it@gmail.com>
2026-05-30 13:09:19 +0200

    New: added OpenVPN files for CE1

--- scripts/VPN Site 1/CE1/root/init.sh
+++ scripts/VPN Site 1/CE1/root/init.sh
@@ -5,7 +5,7 @@ ip addr add 10.1.1.2/30 dev eth1
 ip addr add 192.168.1.1/24 dev eth0
 ip route add default via 10.1.1.1
 
-sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
 openvpn --config /root/client1.conf --daemon
 
